e45891688ff687d52e8850181a17374ce12611f0
GerGG <germain.gg90@gmail.com>
2024-03-20 17:51:24 -0400

    add localstorage middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersSliceReducer from "./users/usersSlice";
 
+const databaseMiddleware = (store) => (next) => (action) => {
+  next(action);
+  localStorage.setItem("users", JSON.stringify(store.getState()));
+};
+
 export const store = configureStore({
   reducer: {
     users: usersSliceReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(databaseMiddleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
